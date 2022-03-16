ece83638f5d5948110dffad01ef47e67746788ea
d.akulov <d.akulov@aix.kz>
2022-03-16 21:47:53 +0600

    added Threads for evaluate lines

--- src/main/java/ru/digitalhabbits/homework2/CalculateAndReplaceJob.java
+++ src/main/java/ru/digitalhabbits/homework2/CalculateAndReplaceJob.java
@@ -1,20 +1,22 @@
+
 package ru.digitalhabbits.homework2;
 
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.concurrent.Callable;
 
 public class CalculateAndReplaceJob implements Callable<Pair<String, Integer>> {
-    private ArrayList<String> lineList;
+    private LinkedList<String> lineList;
     private LineCounterProcessor lineCounterProcessor;
-    public CalculateAndReplaceJob(ArrayList<String> lineList) {
+    public CalculateAndReplaceJob(LinkedList<String> lineList) {
         this.lineList = lineList;
         lineCounterProcessor = new LineCounterProcessor();
     }
 
     @Override
     public Pair<String, Integer> call() throws Exception {
-        return lineCounterProcessor.process(lineList.remove(0));
+        return lineCounterProcessor.process(lineList.removeFirst());
     }
 }
--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,18 +1,20 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
-import org.apache.commons.io.IOUtils;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -27,31 +29,32 @@ public class FileProcessor {
 
         final File file = new File(processingFileName);
         // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+        final File resultFile = new File(resultFileName);
         var fileWriterThread = new Thread(new FileWriter());
         fileWriterThread.start();
+
         var executorService = Executors.newFixedThreadPool(CHUNK_SIZE);
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
             while (scanner.hasNext()) {
                 // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
-                var lineList = new ArrayList<String>(CHUNK_SIZE);
+                var lineList = new LinkedList<String>();
                 var convertedToPairList = new ArrayList<Pair<String, Integer>>(CHUNK_SIZE);
                 for (int i = 0; i < CHUNK_SIZE; i++) {
                     if (scanner.hasNext()) {
-                        lineList.add(scanner.nextLine());
+                        lineList.addLast(scanner.nextLine());
                     }
                 }
                 // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
                 while (lineList.size() > 0) {
                     Future<Pair<String, Integer>> calculatedLine = executorService.submit(new CalculateAndReplaceJob(lineList));
                     convertedToPairList.add(calculatedLine.get());
-                }
-                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
-                while (convertedToPairList.size() > 0) {
                     Pair<String, Integer> pairToWrite = convertedToPairList.remove(0);
                     String content = IOUtils.toString(new FileInputStream(file), defaultCharset());
                     content = content.replaceAll(pairToWrite.getLeft(), pairToWrite.getLeft()) + " " + pairToWrite.getRight();
-                    IOUtils.write(content, new FileOutputStream(file), defaultCharset());
+                    IOUtils.write(content, new FileOutputStream(resultFile), defaultCharset());
                 }
+                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
+
             }
         } catch (ExecutionException e) {
             logger.error("Error occurred during extract value of Future inside FileProcessor.process()", e);
@@ -60,9 +63,9 @@ public class FileProcessor {
         } catch (IOException exception) {
             logger.error("IOException occurred", exception);
         }
+
         // TODO: NotImplemented: остановить поток writerThread
         executorService.shutdown();
-
         logger.info("Finish main thread {}", Thread.currentThread().getName());
     }
 
